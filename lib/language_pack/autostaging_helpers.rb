aaabbfd6e8db89d227d2e250774bfd17f9187237
Ramnivas Laddad <rladdad@gopivotal.com>
2013-06-04 13:04:17 -0700

    Update spring-autoreconfiguration
    Previously, the version of spring-autoreconfiguration that was used by
    the buildpack was incompatible with Cloud Foundry V2.  This change
    updates the version of spring-autoreconfiguration in order to become
    compatible.
    
    [#49892695]

--- lib/language_pack/autostaging_helpers.rb
+++ lib/language_pack/autostaging_helpers.rb
@@ -1,10 +1,11 @@
 module LanguagePack::AutostagingHelpers
 
-  AUTOSTAGING_JAR = "auto-reconfiguration-0.6.6.jar"
+  AUTOSTAGING_JAR = "auto-reconfiguration-0.6.7.jar"
+  REPO_URL = "https://s3.amazonaws.com/maven.springframework.org/milestone/org/cloudfoundry/auto-reconfiguration/0.6.7"
 
   def copy_autostaging_jar(destination_dir)
     FileUtils.chdir(destination_dir) do
-      fetch_package AUTOSTAGING_JAR
+      fetch_package(AUTOSTAGING_JAR, REPO_URL)
     end
   end
 end
